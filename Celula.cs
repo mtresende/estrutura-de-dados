cb8847db11bcfd4a89c19c20dfd73af07b2ff3f6
Matheus Resende <matheus.resende2003@gmail.com>
2025-06-08 12:08:10 -0300

    criando classe celula

---
+++ Celula.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Celula<T>
+{
+    public T item;
+    public Celula<T> prox;
+
+    public Celula()
+    {
+        item = default(T);
+        prox = null;
+    }
+
+    public Celula(T valorItem)
+    {
+        item = valorItem;
+        prox = null;
+    }
+
+    public Celula(T valorItem, Celula<T> valorProx)
+    {
+        item = valorItem;
+        prox = valorProx;
+    }
+}
