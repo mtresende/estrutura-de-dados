92637216cc1b99ef6f4627fdb0237f813fb789ea
Matheus Resende <matheus.resende2003@gmail.com>
2025-06-08 16:04:43 -0300

    adidionando os métodos peek e dequeue à fila

--- DataStructures/Fila.cs
+++ DataStructures/Fila.cs
@@ -12,22 +12,36 @@ public class Fila<T>
         tras = frente;
     }
 
-    public bool EstaVazia() => frente == tras;
-
-    public void Enfileira(T valorItem)
+    public void Enfileira(T valorItem) //Enqueue
     {
         tras.prox = new Celula<T>(valorItem);
         tras = tras.prox;
+        qtde++;
     }
 
-    public T Desenfileira()
+    public T Desenfileira() // Dequeue
     {
         if (frente != tras)
         {
             frente = frente.prox;
             T item = frente.item;
+            qtde --;
             return item;
         }
         return default(T);
+    }
+
+    public T Espia() // Peek
+    {
+        if (frente != tras)
+        {
+            return frente.prox.item;
+        }
+        return default(T);
+    }
+
+    public int Contador() // Count
+    {
+        return qtde;
     }
 }
---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Fila<int> fila = new Fila<int>();
+        fila.Enfileira(1);
+        fila.Enfileira(2);
+        fila.Enfileira(3);
+        fila.Enfileira(4);
+        fila.Enfileira(5);
+
+        int tamanhoFila = fila.Contador();
+
+        Console.WriteLine("Quantidade de itens: " + tamanhoFila + "\n");
+
+        for (int i = 0; i < tamanhoFila; i++)
+        {
+            Console.WriteLine(fila.Desenfileira());
+        }
+    }
+}
