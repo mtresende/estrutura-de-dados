c69755c42873c0da682a742b4b6d797614478244
Matheus Resende <matheus.resende2003@gmail.com>
2025-06-14 18:44:43 -0300

    adicionando os metodos push e pop

--- DataStructures/Pilha.cs
+++ DataStructures/Pilha.cs
@@ -3,4 +3,21 @@ using System;
 public class Pilha<T> {
     private Celula<T> topo = null;
     private int qtde = 0;
+
+    public void Empilha(T valorItem) { // Push
+        topo = new Celula<T>(valorItem, topo);
+        qtde++;
+    }
+
+    public T Desempilha() { // Pop
+        if (topo != null) {
+            T item = topo.item;
+            topo = topo.prox;
+            qtde--;
+            return item;
+        }
+        return default(T);
+    }
+    
+    
 }
